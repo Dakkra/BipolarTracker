e13a80cb6885c7d2af8dd88e998b2ce138cf4964
Chris Soderquist <contact@dakkra.com>
2022-07-09 22:57:35 -0600

    Init gradle project

---
+++ src/main/java/com/dakkra/BipolarTracker/BipolarTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.dakkra.BipolarTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BipolarTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BipolarTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dakkra/BipolarTracker/BipolarTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dakkra.BipolarTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BipolarTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
